381a9a4cd05ab4b598a3ef4cab0a914c77c20b56
Alexander <Aliaksandr_Samchuk@epam.com>
2020-12-23 15:06:23 +0000

    feat: ✨ added Parser implementation

--- lib/parser.rb
+++ lib/parser.rb
@@ -5,5 +5,39 @@ require 'presenters/console_presenter'
 
 module LogsParser
   class Parser
+    def initialize(
+      path,
+      adapter = Adapters::FileAdapter,
+      presenter = Presenters::ConsolePresenter
+    )
+      @path = path
+      @adapter = adapter
+      @presenter = presenter
+    end
+    
+    def call
+      unless path_present?
+        puts "It seem's you aren't provided Path to file!"
+        return
+      end
+    
+      present
+    end
+    
+    private
+    
+    attr_reader :path, :adapter, :presenter
+    
+    def path_present?
+      !!path
+    end
+    
+    def present
+      presenter.new(collect_data).call
+    end
+    
+    def collect_data
+      adapter.new(path).call
+    end
   end
 end
