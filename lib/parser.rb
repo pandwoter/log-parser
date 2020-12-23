899b6b5a2de8511ed61d9c52dec69d2875951435
Alexander <Aliaksandr_Samchuk@epam.com>
2020-12-23 16:02:20 +0000

    feat: ✨ added parser implementation

--- lib/parser.rb
+++ lib/parser.rb
@@ -1,43 +1,46 @@
 # frozen_string_literal: true
 
 require 'adapters/file_adapter'
-require 'presenters/console_presenter'
+require 'printers/visits_printer'
+require 'printers/artifacts_printer'
 
 module LogsParser
   class Parser
     def initialize(
       path,
       adapter = Adapters::FileAdapter,
-      presenter = Presenters::ConsolePresenter
+      printers = [Printers::VisitsPrinter, Printers::ArtifactsPrinter]
     )
       @path = path
       @adapter = adapter
-      @presenter = presenter
+      @printers = printers
     end
-    
+
     def call
       unless path_present?
         puts "It seem's you aren't provided Path to file!"
         return
       end
-    
-      present
+
+      print_result
     end
-    
+
     private
-    
-    attr_reader :path, :adapter, :presenter
-    
+
+    attr_reader :path, :adapter, :printers
+
     def path_present?
       !!path
     end
-    
-    def present
-      presenter.new(collect_data).call
+
+    def print_result
+      return unless collected_data
+
+      printers.each { |p| p.new(collected_data).call }
     end
-    
-    def collect_data
-      adapter.new(path).call
+
+    def collected_data
+      @collected_data ||= adapter.new(path).call
     end
   end
 end
